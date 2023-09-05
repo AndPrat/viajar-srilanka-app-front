008e02792e96da54a712157d1297dfd546f5f413
Andrea Prat <andr8ide@gmail.com>
2023-09-05 14:05:22 +0200

    Create Header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,19 @@
+import "./Header.css";
+
+const Header = (): React.ReactElement => {
+  return (
+    <header className="header">
+      <div className="header__logo">
+        <img
+          src="/img/logo-viajarSriLanka.png"
+          alt="Viajar a Sri Lanka logo"
+          width="32"
+          height="32"
+        ></img>
+      </div>
+      <h1 className="header__title">Viajar a Sri Lanka</h1>
+    </header>
+  );
+};
+
+export default Header;
