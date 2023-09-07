e3dda1c355c9e71b80b722d09eb4ae81d1d23369
Andrea Prat <andr8ide@gmail.com>
2023-09-07 23:30:30 +0200

    Create PlacesListPage component

---
+++ src/pages/PlacesListPage/PlacesListPage.tsx
@@ -0,0 +1,7 @@
+import "./PlacesListPage.css";
+
+const PlacesListPage = (): React.ReactElement => {
+  return <h2 className="places__title">Lugares de interés</h2>;
+};
+
+export default PlacesListPage;
