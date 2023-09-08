120550feb6a037c0bf45e26124b5f77be89a83d3
Andrea Prat <andr8ide@gmail.com>
2023-09-08 11:02:38 +0200

    Add routes in App component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,30 @@
 import Header from "../Header/Header";
-import NavigationBar from "../NavigationBar/NavigationBar";
 import Homepage from "../../pages/Homepage/Homepage";
+import { Navigate, Route, Routes } from "react-router-dom";
+import paths from "../../routers/paths/paths";
+import PlacesListPage from "../../pages/PlacesListPage/PlacesListPage";
+import ProtectedRoute from "../ProtectedRoute/ProtectedRoute";
+import NavigationBar from "../NavigationBar/NavigationBar";
 
 const App = (): React.ReactElement => {
   return (
     <div className="container">
       <Header />
       <main className="main-content">
-        <Homepage />
-        <NavigationBar />
+        <Routes>
+          <Route path={paths.homePage} element={<Homepage />} />
+          <Route
+            path={paths.places}
+            element={
+              <ProtectedRoute>
+                <PlacesListPage />
+                <NavigationBar />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route path="/" element={<Navigate to={paths.places} />} />
+        </Routes>
       </main>
     </div>
   );
