a069ae121b58636b6ec00735ccbd894b89248f0f
Andrea Prat <andr8ide@gmail.com>
2023-09-09 18:02:49 +0200

    Fix the center position of NavigationBar links

--- src/pages/PlacesListPage/PlacesListPage.tsx
+++ src/pages/PlacesListPage/PlacesListPage.tsx
@@ -1,7 +1,7 @@
 import "./PlacesListPage.css";
 
 const PlacesListPage = (): React.ReactElement => {
-  return <h2 className="places__title">Lugares de interés</h2>;
+  return <h1 className="places__title">Lugares de interés</h1>;
 };
 
 export default PlacesListPage;
