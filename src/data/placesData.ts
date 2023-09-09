3ae07ddc25a8c822b1b9ca6b02a14e189d493f3a
Andrea Prat <andr8ide@gmail.com>
2023-09-09 20:09:26 +0200

    Add loadPlaces in PlacesListPage with placesData

---
+++ src/data/placesData.ts
@@ -0,0 +1,30 @@
+import { Place } from "../types";
+
+const placesData: Place[] = [
+  {
+    id: "1",
+    name: "Sigiriya",
+    subtitle: "Templo de la roca del león",
+    location: "Matale",
+    schedule: "8h a 17h",
+    otherRelatedPlace: "Mirador Pidurangala",
+    description:
+      "La inmensa e imponente Roca del León o Lion Rock de Sigiriya, nacida de una erupción volcánica, emerge como un titán en el paisaje de la ciudad, que ineludiblemente ha ligado su historia a ella.",
+    image:
+      "https://www.storiesbysoumya.com/wp-content/uploads/2021/11/sigiriya-rock-fortress-768x432.jpg",
+  },
+  {
+    id: "2",
+    name: "Ahas Namaye Palama",
+    subtitle: "Tren de Kandy a Ella",
+    location: "Ella",
+    schedule: "7h a 20h",
+    otherRelatedPlace: "Demodara y Ella",
+    description:
+      "La frondosa selva y los campos de té rodean casi engullendo el Nine Arch Bridge, creando una atmósfera de belleza cuyo cénit es cuando aparece un tren desde la boca del túnel. El puente fue una de las obras de ingeniería y arquitectura más admiradas del periodo colonial británico en la isla de Ceilán.",
+    image:
+      "https://www.got2globe.com/wp-content/uploads/2020/02/Composica%CC%83o-Nine-Arches-Bridge-Ella-Sri-Lanka.jpg",
+  },
+];
+
+export default placesData;
