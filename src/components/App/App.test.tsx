ab5c3011dc3e87ccfd3f721fadb05703722ddaa4
Andrea Prat <andr8ide@gmail.com>
2023-09-09 20:11:06 +0200

    Refactor App and PlacesListPage tests with provider store

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -3,6 +3,9 @@ import { User } from "firebase/auth";
 import auth, { AuthStateHook } from "react-firebase-hooks/auth";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { setupStore } from "../../store";
+import { Provider } from "react-redux";
+import { placesMock } from "../../mocks/placeMock";
 
 vi.mock("firebase/auth");
 
@@ -11,6 +14,12 @@ const user: Partial<User> = { displayName: "Oscar" };
 const authStateHookMock: Partial<AuthStateHook> = [user as User];
 auth.useAuthState = vi.fn().mockReturnValue(authStateHookMock);
 
+const store = setupStore({
+  placesState: {
+    places: placesMock,
+  },
+});
+
 describe("Given an App component", () => {
   describe("When it is rendered", () => {
     test("Then it should show the text 'Viajar a Sri Lanka' in the heading", () => {
@@ -18,7 +27,9 @@ describe("Given an App component", () => {
 
       render(
         <BrowserRouter>
-          <App />
+          <Provider store={store}>
+            <App />
+          </Provider>
         </BrowserRouter>,
       );
 
