0cf7b8fc74f1d660df105a7d28c461a583bbaeeb
Andrea Prat <andr8ide@gmail.com>
2023-09-10 13:41:49 +0200

    Fix the NavigationBar styles when there is user or not

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,12 +5,16 @@ import paths from "../../routers/paths/paths";
 import PlacesListPage from "../../pages/PlacesListPage/PlacesListPage";
 import NavigationBar from "../NavigationBar/NavigationBar";
 import ProtectedRoute from "../ProtectedRoute/ProtectedRoute";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../../firebase";
 
 const App = (): React.ReactElement => {
+  const [user] = useAuthState(auth);
+
   return (
     <div className="container">
       <Header />
-      <main className="main-content">
+      <main className={`main-content ${user ? "has-nav" : null}`}>
         <Routes>
           <Route path={paths.homePage} element={<Homepage />} />
           <Route
