c151b9b399f6f7a5abb9e76c4b48e1cb52d8a63e
Andrea Prat <andr8ide@gmail.com>
2023-09-11 22:49:57 +0200

    Refactor size and format images

--- src/data/placesData.ts
+++ src/data/placesData.ts
@@ -11,7 +11,7 @@ const placesData: Place[] = [
     description:
       "La inmensa e imponente Roca del León o Lion Rock de Sigiriya, nacida de una erupción volcánica, emerge como un titán en el paisaje de la ciudad, que ineludiblemente ha ligado su historia a ella.",
     image:
-      "https://www.storiesbysoumya.com/wp-content/uploads/2021/11/sigiriya-rock-fortress-768x432.jpg",
+      "https://res.cloudinary.com/ds7klnuyj/image/upload/v1694464858/Sri-Lanka-places/lugar-sigiriya.jpg",
   },
   {
     id: "2",
@@ -23,7 +23,7 @@ const placesData: Place[] = [
     description:
       "La frondosa selva y los campos de té rodean casi engullendo el Nine Arch Bridge, creando una atmósfera de belleza cuyo cénit es cuando aparece un tren desde la boca del túnel. El puente fue una de las obras de ingeniería y arquitectura más admiradas del periodo colonial británico en la isla de Ceilán.",
     image:
-      "https://estaticos-cdn.prensaiberica.es/clip/53b129d8-dfe1-44d9-a544-da6a8267b028_source-aspect-ratio_default_0.jpg",
+      "https://res.cloudinary.com/ds7klnuyj/image/upload/v1694464851/Sri-Lanka-places/lugar-ahas-namaye-palama.jpg",
   },
 ];
 
