d1a4cba7b81eeba269e58ec6c33b473aefd97966
Andrea Prat <andr8ide@gmail.com>
2023-09-11 11:50:33 +0200

    Test PlaceListPage component when tere is not PlaceCard saved

--- src/pages/PlacesListPage/PlacesListPage.test.tsx
+++ src/pages/PlacesListPage/PlacesListPage.test.tsx
@@ -1,12 +1,20 @@
 import { render, screen } from "@testing-library/react";
 import PlacesListPage from "./PlacesListPage";
+import { setupStore } from "../../store";
+import { placesMock } from "../../mocks/placeMock";
 import { Provider } from "react-redux";
-import { store } from "../../store";
 
 describe("Given a PlacesListPage component", () => {
+  vi.mock("react", () => ({ useEffect: vi.fn() }));
+
   describe("When it is rendered", () => {
     test("Then it should shouw a heading with the text 'Lugares de interés'", () => {
       const expectedHeadingText = "Lugares de interés";
+      const store = setupStore({
+        placesState: {
+          places: placesMock,
+        },
+      });
 
       render(
         <Provider store={store}>
@@ -19,6 +27,30 @@ describe("Given a PlacesListPage component", () => {
       });
 
       expect(heading).toBeInTheDocument();
+    });
+  });
+
+  describe("When it is rendered without place card", () => {
+    test("Then nit should show a heading with 'No hay ningún lugar guardado' text", () => {
+      const expectHeadingText = "No hay ningún lugar guardado";
+
+      const store = setupStore({
+        placesState: {
+          places: [],
+        },
+      });
+
+      render(
+        <Provider store={store}>
+          <PlacesListPage />
+        </Provider>,
+      );
+
+      const headingText = screen.getByRole("heading", {
+        name: expectHeadingText,
+      });
+
+      expect(headingText).toBeInTheDocument();
     });
   });
 });
