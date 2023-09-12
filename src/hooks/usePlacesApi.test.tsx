a1d8dd9d5009ebf56ed4d74d8cbf7837ac5a8644
Andrea Prat <andr8ide@gmail.com>
2023-09-12 13:04:05 +0200

    Test usePlacesApi custom hook with apiPlacesMock mock

---
+++ src/hooks/usePlacesApi.test.tsx
@@ -0,0 +1,37 @@
+import { renderHook } from "@testing-library/react";
+import usePlacesApi from "./usePlacesApi";
+import { placesMock } from "../mocks/placeMock";
+import { server } from "../mocks/server";
+import { errorHandlers } from "../mocks/handlers";
+
+describe("Given a function getPlaces", () => {
+  describe("When the function is called", () => {
+    test("Then it should receives a list of places", async () => {
+      const {
+        result: {
+          current: { getPlaces },
+        },
+      } = renderHook(() => usePlacesApi());
+
+      const places = await getPlaces();
+
+      expect(places).toStrictEqual(placesMock);
+    });
+  });
+
+  test("Then it should throw an error 'Couldn't load places' when rejecting", () => {
+    server.resetHandlers(...errorHandlers);
+
+    const expectedError = new Error("Couldn't load places");
+
+    const {
+      result: {
+        current: { getPlaces },
+      },
+    } = renderHook(() => usePlacesApi());
+
+    const places = getPlaces();
+
+    expect(places).rejects.toThrowError(expectedError);
+  });
+});
