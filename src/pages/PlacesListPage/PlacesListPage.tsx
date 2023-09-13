34b64029cbc459b572d87d14a42922c4ac91d410
Andrea Prat <andr8ide@gmail.com>
2023-09-13 16:42:20 +0200

    Add isLoadingAuth in PlaesListPage to Loading charge and Lazy loading

--- src/pages/PlacesListPage/PlacesListPage.tsx
+++ src/pages/PlacesListPage/PlacesListPage.tsx
@@ -1,4 +1,4 @@
-import { Suspense, useEffect } from "react";
+import { lazy, useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../../store";
 import "./PlacesListPage.css";
 import { loadPlacesActionCreator } from "../../store/places/placesSlice";
@@ -8,10 +8,14 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../../firebase";
 import Loading from "../../components/Loading/Loading";
 
+export const LazyPlaceLisPage = lazy(
+  () => import("../../pages/PlacesListPage/PlacesListPage.js"),
+);
+
 const PlacesListPage = (): React.ReactElement => {
   const places = useAppSelector((state) => state.placesState.places);
-  const isLoading = useAppSelector((state) => state.uiState.isLoading);
-  const [user] = useAuthState(auth);
+  const isLoadingUi = useAppSelector((state) => state.uiState.isLoading);
+  const [user, isLoadingAuth] = useAuthState(auth);
 
   const dispatch = useAppDispatch();
   const { getPlaces } = usePlacesApi();
@@ -29,16 +33,13 @@ const PlacesListPage = (): React.ReactElement => {
   return (
     <>
       <h1 className="places__title">Lugares de interés</h1>
-      {isLoading && <Loading />}
-      {places.length > 0 ? (
-        <Suspense>
-          <PlacesList />
-        </Suspense>
-      ) : (
-        <Suspense>
-          <h2 className="places__text">No hay ningún lugar guardado</h2>
-        </Suspense>
-      )}
+      {isLoadingUi && <Loading />}
+      {places.length > 0
+        ? !isLoadingUi && !isLoadingAuth && <PlacesList />
+        : !isLoadingUi &&
+          !isLoadingAuth && (
+            <h2 className="places__text">No hay ningún lugar guardado</h2>
+          )}
     </>
   );
 };
