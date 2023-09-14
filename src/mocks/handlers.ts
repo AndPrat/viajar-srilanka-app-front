f545de41b7ea05c701c875819e2c1ccd8e51f748
Andrea Prat <andr8ide@gmail.com>
2023-09-14 17:50:34 +0200

    Fix lugares naming endpoint to places

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -2,11 +2,11 @@ import { rest } from "msw";
 import { apiPlacesMock, placeIdMock, wrongPlaceIdMock } from "./placeMock";
 
 export const handlers = [
-  rest.get(`${import.meta.env.VITE_API_URL}/lugares`, (_req, res, ctx) => {
+  rest.get(`${import.meta.env.VITE_API_URL}/places`, (_req, res, ctx) => {
     return res(ctx.status(200), ctx.json(apiPlacesMock));
   }),
   rest.delete(
-    `${import.meta.env.VITE_API_URL}/lugares/${placeIdMock}`,
+    `${import.meta.env.VITE_API_URL}/places/${placeIdMock}`,
     (_req, res, ctx) => {
       return res(
         ctx.status(200),
@@ -17,11 +17,11 @@ export const handlers = [
 ];
 
 export const errorHandlers = [
-  rest.get(`${import.meta.env.VITE_API_URL}/lugares`, (_req, res, ctx) => {
+  rest.get(`${import.meta.env.VITE_API_URL}/places`, (_req, res, ctx) => {
     return res(ctx.status(404, "Can't get places right now"));
   }),
   rest.delete(
-    `${import.meta.env.VITE_API_URL}/lugares/${wrongPlaceIdMock}`,
+    `${import.meta.env.VITE_API_URL}/places/${wrongPlaceIdMock}`,
     (_res, res, ctx) => {
       return res(ctx.status(404, "Can't remove the place"));
     },
