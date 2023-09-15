09dfab3d22a63bd356377237e89b6189c2f3226c
Andrea Prat <andr8ide@gmail.com>
2023-09-15 18:12:04 +0200

    Refactor showFeedback styles

--- src/showFeedback/showFeedback.ts
+++ src/showFeedback/showFeedback.ts
@@ -4,13 +4,7 @@ import "./showFeedback.css";
 
 const showFeedback = (message: string, type: "error" | "success"): void => {
   toast[type](message, {
-    autoClose: 5000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    progress: undefined,
-    theme: "light",
+    position: "top-center",
   });
 };
 
