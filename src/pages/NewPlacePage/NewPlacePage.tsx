84289dad9c341e81e2c3939073d00bf4df995e39
Andrea Prat <andr8ide@gmail.com>
2023-09-16 17:01:57 +0200

    Add actionOnSubmit action in NewPlaceForm

--- src/pages/NewPlacePage/NewPlacePage.tsx
+++ src/pages/NewPlacePage/NewPlacePage.tsx
@@ -1,16 +1,33 @@
 import { lazy } from "react";
-import "./NewPlacePage.css";
 import NewPlaceForm from "../../components/NewPlaceForm/NewPlaceForm.js";
+import usePlacesApi from "../../hooks/usePlacesApi.js";
+import { useAppDispatch } from "../../store/index.js";
+import { addPlaceActionCreator } from "../../store/places/placesSlice.js";
+import { Place } from "../../types.js";
+import "./NewPlacePage.css";
+import { useNavigate } from "react-router-dom";
+import paths from "../../routers/paths/paths.js";
 
 export const LazyNewPlacePage = lazy(
   () => import("../../pages/NewPlacePage/NewPlacePage.js"),
 );
 
 const NewPlacePage = (): React.ReactElement => {
+  const { addPlace } = usePlacesApi();
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const actionOnSubmit = async (newPlaceToAdd: Omit<Place, "id">) => {
+    const newPlace = await addPlace(newPlaceToAdd);
+
+    dispatch(addPlaceActionCreator(newPlace));
+    navigate(paths.places);
+  };
+
   return (
     <>
       <h1 className="newPlace__title">Añade un lugar</h1>
-      <NewPlaceForm />
+      <NewPlaceForm actionOnSubmit={actionOnSubmit} />
     </>
   );
 };
