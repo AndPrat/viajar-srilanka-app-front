f5c8e616088d42311babe903753b56a1554e3aa3
Andrea Prat <andr8ide@gmail.com>
2023-09-19 18:22:16 +0200

    Test toggle button in App component with mock

--- src/mocks/placeMock.ts
+++ src/mocks/placeMock.ts
@@ -160,3 +160,32 @@ export const placeMockModify: Place = {
     "https://res.cloudinary.com/ds7klnuyj/image/upload/v1694588057/Sri-Lanka-places/lugar-sigiriya.webp",
   isFavorite: true,
 };
+
+export const placesMockModify: Place[] = [
+  {
+    id: "64fb41416d0350ec52f38917",
+    name: "Sigiriya",
+    subtitle: "Templo de la roca del león",
+    location: "Matale",
+    schedule: "8h a 17h",
+    otherRelatedPlace: "Mirador Pidurangala",
+    description:
+      "La inmensa e imponente Roca del León o Lion Rock de Sigiriya, nacida de una erupción volcánica, emerge como un titán en el paisaje de la ciudad, que ineludiblemente ha ligado su historia a ella.",
+    image:
+      "https://res.cloudinary.com/ds7klnuyj/image/upload/v1694588057/Sri-Lanka-places/lugar-sigiriya.webp",
+    isFavorite: true,
+  },
+  {
+    id: "64fb42046d0350ec52f38918",
+    name: "Ahas Namaye Palama",
+    subtitle: "Tren de Kandy a Ella",
+    location: "Ella",
+    schedule: "7h a 20h",
+    otherRelatedPlace: "Demodara y Ella",
+    description:
+      "La frondosa selva y los campos de té rodean casi engullendo el Nine Arch Bridge, creando una atmósfera de belleza cuyo cénit es cuando aparece un tren desde la boca del túnel. El puente fue una de las obras de ingeniería y arquitectura más admiradas del periodo colonial británico en la isla de Ceilán.",
+    image:
+      "https://www.got2globe.com/wp-content/uploads/2020/02/Composica%CC%83o-Nine-Arches-Bridge-Ella-Sri-Lanka.jpg",
+    isFavorite: false,
+  },
+];
