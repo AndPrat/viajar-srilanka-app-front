d375218e4018db4e9d5062dc8e2073fc91098537
Andrea Prat <andr8ide@gmail.com>
2023-10-05 18:46:36 +0200

    Add hero images and heroImages file

---
+++ src/heroImages/heroImages.tsx
@@ -0,0 +1,33 @@
+const fallbackHeroImagePlacesList = "/img/background-image.jpg";
+const desktopWebpHeroImagePlacesList = "/img/background-image.webp";
+const desktopHeroImagePlacesList = "/img/background-image.jpg";
+const mobileWebpHeroImagePlacesList = "/img/hero-background-m.webp";
+const mobileHeroImagePlacesList = "/img/hero-background-m.jpg";
+const altHeroImagePlacesList = "Vistas de Sigiriya en el amanecer";
+const titleHeroImagePlacesList = "Tu viaje a Sri Lanka empieza aquí";
+
+const fallbackHeroImageNewPlace = "/img/background-heroNewPlace.jpg";
+const desktopWebpHeroImageNewPlace = "/img/background-heroNewPlace.webp";
+const desktopHeroImageNewPlace = "/img/background-heroNewPlace.jpg";
+const mobileWebpHeroImageNewPlace = "/img/background-heroNewPlace-m.webp";
+const mobileHeroImageNewPlace = "/img/background-heroNewPlace-m.jpg";
+const altHeroImageNewPlace =
+  "Una de las entradas al Templo Budista Gangaramaya de Colombo, Sri Lanka";
+const titleHeroImageNewPlace = "";
+
+export {
+  fallbackHeroImagePlacesList,
+  desktopWebpHeroImagePlacesList,
+  desktopHeroImagePlacesList,
+  mobileWebpHeroImagePlacesList,
+  mobileHeroImagePlacesList,
+  altHeroImagePlacesList,
+  titleHeroImagePlacesList,
+  fallbackHeroImageNewPlace,
+  desktopWebpHeroImageNewPlace,
+  desktopHeroImageNewPlace,
+  mobileWebpHeroImageNewPlace,
+  mobileHeroImageNewPlace,
+  altHeroImageNewPlace,
+  titleHeroImageNewPlace,
+};
